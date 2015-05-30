02b4bf38d0b3418710c3ea69eec7d61c046a9b2d
Nidhish Nair <nnair@epsilon.com>
2015-05-29 21:49:57 -0500

    instructions to run

--- src/main/java/com/mine/nidhish/GridRunner.java
+++ src/main/java/com/mine/nidhish/GridRunner.java
@@ -1,6 +1,11 @@
 package com.mine.nidhish;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Set;
 
 /**
@@ -18,93 +23,123 @@ public class GridRunner {
 
     public GridRunner() {
 
-        loadDictionary();
+	//        System.out.println("Starting Grid Scanning ");
+	//        long startTime = System.currentTimeMillis();
 
+	loadDictionary();
 
-        InputStream in = this.getClass().getClassLoader()
-		        .getResourceAsStream("grid.txt");
+	CharacterGrid characterGrid = readCharacterGrid();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+	Set<String> wordSet = characterGrid.findWordMatchingDictionary(dictionary);
 
+	System.out.println(wordSet.size());
+	for (String word : wordSet) {
+	    System.out.println(word);
+	}
 
+	//        System.out.println("Finish scanning character grid in " + (System.currentTimeMillis() - startTime) + " ms");
 
-
-        try {
-
-            String firstLineOfGridFile = br.readLine();
-
-            String[] rowAndColumn = firstLineOfGridFile.split(" ");
-
-            int numberOfRows = Integer.parseInt(rowAndColumn[0]);
-            int numberOfColumns = Integer.parseInt(rowAndColumn[1]);
-
-            String gridLine;
-            int currentRowNumber =0;
-
-            char[][] rowCharArray = new char[numberOfRows][numberOfColumns];
-
-	    while ((gridLine = br.readLine()) != null) {
-	        String[] row = gridLine.split(" ");
-	        if (row.length!=numberOfColumns){
-	            throw new RuntimeException("Invalid grid file at line number " + currentRowNumber+1);
-	        }
-
-	        char[] colCharArray = new char[numberOfColumns];
-
-	        for (int i = 0; i < row.length; i++) {
-	            colCharArray[i] = Character.toLowerCase(row[i].charAt(0));
-	        }
-	        rowCharArray[currentRowNumber] = colCharArray;
-	        currentRowNumber++;
-	    }
-
-            if (currentRowNumber != numberOfRows){
-                throw new RuntimeException("Invalid grid file wrong number of rows found " + currentRowNumber);
-            }
-
-
-	    br.close();
-
-            CharacterGrid characterGrid = new CharacterGrid(numberOfRows,numberOfColumns,rowCharArray);
-
-            Set<String> wordSet = characterGrid.findWordMatchingDictionary(dictionary);
-
-            System.out.println(wordSet.size());
-            for (String word :wordSet){
-                System.out.println(word);
-            }
-
-        }catch (Exception ex){
-	    throw new RuntimeException(ex);
-        }
     }
 
-    private void loadDictionary() {
-        dictionary = new Dictionary();
+    private CharacterGrid readCharacterGrid() {
 
-	InputStream in = this.getClass().getClassLoader()
-			.getResourceAsStream("englishwords.txt");
-
-	BufferedReader br = new BufferedReader(new InputStreamReader(in));
-
-	String word;
-
-	System.out.println("Starting to load dictionary");
-
+	//        System.out.println("Starting to read Grid ");
+	//
 	long startTime = System.currentTimeMillis();
 
 	try {
+	    InputStream in;
 
-	    while ((word = br.readLine()) != null) {
-	        word = word.replaceAll("[^a-zA-Z]", "");
-		dictionary.addWord(word);
+	    if (System.getProperty("gridFile") != null) {
+		String gridFileName = System.getProperty("gridFile");
+		Path path = Paths.get(gridFileName);
+		in = Files.newInputStream(path);
+	    } else {
+		in = this.getClass().getClassLoader().getResourceAsStream("grid.txt");
 	    }
+
+	    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+	    int numberOfRows;
+	    int numberOfColumns;
+	    char[][] rowCharArray;
+
+	    String firstLineOfGridFile = br.readLine();
+
+	    String[] rowAndColumn = firstLineOfGridFile.split(" ");
+
+	    numberOfRows = Integer.parseInt(rowAndColumn[0]);
+	    numberOfColumns = Integer.parseInt(rowAndColumn[1]);
+
+	    String gridLine;
+	    int currentRowNumber = 0;
+
+	    rowCharArray = new char[numberOfRows][numberOfColumns];
+
+	    while ((gridLine = br.readLine()) != null) {
+		String[] row = gridLine.split(" ");
+		if (row.length != numberOfColumns) {
+		    throw new RuntimeException("Invalid grid file at line number " + currentRowNumber + 1);
+		}
+
+		char[] colCharArray = new char[numberOfColumns];
+
+		for (int i = 0; i < row.length; i++) {
+		    colCharArray[i] = Character.toLowerCase(row[i].charAt(0));
+		}
+		rowCharArray[currentRowNumber] = colCharArray;
+		currentRowNumber++;
+	    }
+
+	    if (currentRowNumber != numberOfRows) {
+		throw new RuntimeException("Invalid grid file wrong number of rows found " + currentRowNumber);
+	    }
+
 	    br.close();
-	}catch (Exception ex){
+
+	    CharacterGrid characterGrid = new CharacterGrid(numberOfRows, numberOfColumns, rowCharArray);
+//	    System.out.println("Finish reading character grid in " + (System.currentTimeMillis() - startTime) + " ms");
+	    return characterGrid;
+	} catch (Exception ex) {
 	    throw new RuntimeException(ex);
 	}
 
-	System.out.println("Loaded dictionary in " + (System.currentTimeMillis() - startTime) + " milliseconds");
+    }
+
+    private void loadDictionary() {
+	try {
+	    dictionary = new Dictionary();
+
+	    String dictionaryFileName = "";
+
+	    InputStream in;
+
+	    if (System.getProperty("dictionaryFile") != null) {
+		dictionaryFileName = System.getProperty("dictionaryFile");
+		Path path = Paths.get(dictionaryFileName);
+		in = Files.newInputStream(path);
+	    } else {
+		in = this.getClass().getClassLoader().getResourceAsStream("englishwords.txt");
+	    }
+
+	    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+	    String word;
+
+	    //	System.out.println("Starting to load dictionary");
+
+	    long startTime = System.currentTimeMillis();
+
+	    while ((word = br.readLine()) != null) {
+		word = word.replaceAll("[^a-zA-Z]", "");
+		dictionary.addWord(word);
+	    }
+	    br.close();
+	} catch (Exception ex) {
+	    throw new RuntimeException(ex);
+	}
+
+	//	System.out.println("Loaded dictionary in " + (System.currentTimeMillis() - startTime) + " milliseconds");
     }
 
 }
